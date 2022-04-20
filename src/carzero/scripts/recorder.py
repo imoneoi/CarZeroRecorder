f1b843ddd48c83cf438028d6d68321c70cbf8749
One <imone@tuta.io>
2022-04-20 18:05:05 +0800

    recorder

--- src/carzero/scripts/recorder.py
+++ src/carzero/scripts/recorder.py
@@ -7,64 +7,57 @@ import numpy as np
 import onnxruntime
 import cv2
 import time
+import json
 
 import rospy
 from dynamic_reconfigure.server import Server
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 
-from carzero.cfg import RLPlannerConfig
 from shared_array import SharedArray
 
 
 # Global Variables & Callback
 
 GLOBAL_ODOM = Odometry()
-GLOBAL_CONFIG = None
 
 
 def odom_callback(data: Odometry):
     global GLOBAL_ODOM
     GLOBAL_ODOM = data
 
+# Recorder worker
 
-def reconfigure_callback(config: RLPlannerConfig, level: int):
-    global GLOBAL_CONFIG
-    GLOBAL_CONFIG = config
-    return config
-
-
-# Main
-
-
-def visualizer_worker_(vis_dict: dict, event: Event, terminate: Event, control_rate: int, record_path: str):
+def visualizer_worker_(vis_dict: dict, event: Event, terminate: Event, record_path: str):
     vis_dict = {k: v.get() for k, v in vis_dict.items()}
     for k in vis_dict.keys():
         cv2.namedWindow(k, cv2.WINDOW_NORMAL)
 
-    video_writers = {}
     if record_path:
         save_path = os.path.join(record_path, time.strftime("%H-%M-%S %m-%d-%Y"))
         os.makedirs(save_path, exist_ok=True)
 
-        video_writers = {
-            k: cv2.VideoWriter(os.path.join(save_path, k + ".mp4"), cv2.VideoWriter_fourcc(*"mp4v"), control_rate,
-                               v.shape[1::-1])
-            for k, v in vis_dict.items()}
-
+    sn = 0
     while not terminate.is_set():
+        sn += 1
+
         event.wait()
         event.clear()
         for k, v in vis_dict.items():
-            image = cv2.cvtColor(v, cv2.COLOR_RGB2BGR)
-            cv2.imshow(k, image)
-            if record_path:
-                video_writers[k].write(image)
+            if len(v.shape) == 3:
+                image = cv2.cvtColor(v, cv2.COLOR_RGB2BGR)
+                if record_path:
+                    cv2.imsave(os.path.join(save_path, "{}_{}.jpg".format(sn, k)))
+                cv2.imshow(k, image)
+            else:
+                if record_path:
+                    with open(os.path.join(save_path, "{}_{}.json".format(sn, k)), "wt") as f:
+                        json.dump(v, f)
+                        f.close()
 
         cv2.waitKey(1)
 
-    for v in video_writers.values():
-        v.release()
+# Segmentation
 
 
 def read_image_size(res: str):
@@ -93,17 +86,16 @@ def run_semantic_segmentation(img: np.ndarray, model: onnxruntime.InferenceSessi
     return np.concatenate([[label == i] for i in range(1, logprob.shape[1])], axis=1).astype(np.uint8) * 255
 
 
+# Main
+
 def main():
     # init node and comm
-    rospy.init_node("rl_planner")
+    rospy.init_node("recorder")
     odom_sub = rospy.Subscriber("odom", Odometry, odom_callback)
-    twist_pub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
-    reconf_server = Server(RLPlannerConfig, reconfigure_callback)
 
     # read models
     model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../models"))
-    model_semseg = onnxruntime.InferenceSession(os.path.join(model_path, rospy.get_param("model_semseg", "ep_44_iou_0.9343.checkpoint.onnx")))
-    model_policy = onnxruntime.InferenceSession(os.path.join(model_path, rospy.get_param("model_policy", "569979_deploy_v0.1.onnx")))
+    model_semseg = onnxruntime.InferenceSession(os.path.join(model_path, rospy.get_param("model_semseg", "ep_44_iou_0.9343.checkpoint.onnx")), providers=["CUDAExecutionProvider"])
 
     # read parameters
     param_loop_rate = rospy.get_param("loop_rate", 10)
@@ -112,24 +104,27 @@ def main():
     param_cam_fps = rospy.get_param("camera_fps", 30)
     param_cap_fourcc = rospy.get_param("camera_fourcc", "YUYV")
 
+    param_max_vel = rospy.get_param("max_vel", 1.5)
+
     param_size_cam = read_image_size(rospy.get_param("size_camera", "640x480"))
     param_size_seg = read_image_size(rospy.get_param("size_seg", "640x320"))
     param_size_policy = read_image_size(rospy.get_param("size_policy", "160x80"))
 
-    param_record_path = rospy.get_param("record_path", "Videos/carzero")
+    param_record_path = rospy.get_param("record_path", "Records/carzero")
 
     # visualizer process
     shared_images_arr = {
         "crop": SharedArray((*param_size_seg[::-1], 3), np.uint8),
         "seg": SharedArray((*param_size_seg[::-1], 3), np.uint8),
-        "policy": SharedArray((*param_size_policy[::-1], 3), np.uint8)
+        "policy": SharedArray((*param_size_policy[::-1], 3), np.uint8),
+        "action": SharedArray((2,), np.float32)
     }
     shared_images = {k: v.get() for k, v in shared_images_arr.items()}
     shared_event = Event()
     terminate_event = Event()
 
     vis_process = Process(target=visualizer_worker_, args=(shared_images_arr, shared_event, terminate_event,
-                                                           param_loop_rate, param_record_path))
+                                                           param_record_path))
     vis_process.start()
 
     # control loop
@@ -157,33 +152,24 @@ def main():
         # semantic segmentation
         seg = run_semantic_segmentation(frame, model_semseg)
         # add speed layer
-        global GLOBAL_ODOM, GLOBAL_CONFIG
+        global GLOBAL_ODOM
         speed = GLOBAL_ODOM.twist.twist.linear
         speed = np.linalg.norm(np.array([speed.x, speed.y, speed.z]))
-        speed = np.clip(speed / GLOBAL_CONFIG.max_vel * 255, 0, 255).astype(np.uint8)
+        speed = np.clip(speed / param_max_vel * 255, 0, 255).astype(np.uint8)
         seg = np.concatenate([np.broadcast_to(speed, (1, 1, *seg.shape[2:])), seg], axis=1)
 
         seg = seg[0].transpose(1, 2, 0)
         np.copyto(shared_images["seg"], seg)
+
         # policy inference
         policy_in = cv2.resize(seg, param_size_policy)
         np.copyto(shared_images["policy"], policy_in)
 
-        policy = model_policy.run(None, {"obs": np.expand_dims(policy_in.transpose(2, 0, 1), [0, 1])})[0]
-        accel, steer = policy[0]
-        print(policy)
-        # output
-        if accel < 0:
-            converted_accel = GLOBAL_CONFIG.max_brake * accel - GLOBAL_CONFIG.vel_decay
-        else:
-            converted_accel = GLOBAL_CONFIG.max_accel * accel - GLOBAL_CONFIG.vel_decay
-
-        twist.angular.z = GLOBAL_CONFIG.max_steer * -steer  # steer > 0 is turn right
-        twist.linear.x = np.clip(twist.linear.x + converted_accel * dt, 0, GLOBAL_CONFIG.max_vel)
-        twist_pub.publish(twist)
+        # action
+        np.copyto(shared_images["action"], np.array([GLOBAL_ODOM.twist.twist.linear, GLOBAL_ODOM.twist.twist.angular.z]))
 
         # loop
-        # print("Latency: {:.2f} ms".format((time.time() - time_start) * 1000))
+        print("Latency: {:.2f} ms".format((time.time() - time_start) * 1000))
         shared_event.set()
         rate.sleep()
 
